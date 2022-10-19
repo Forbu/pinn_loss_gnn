6f8691917bbc270de2b91bf0796f2f5e00236d0b
Forbu <adri141193@yahoo.fr>
2022-10-19 14:14:36 +0200

    adding trainer class

---
+++ pinn_loss/trainer.py
@@ -0,0 +1,30 @@
+import jax 
+import jax.numpy as jnp
+
+import flax
+from flax import nn
+
+class trainer_flax:
+    """
+    Class that manage the flax training
+    """
+    def __init__(self):
+        pass
+    
+    def training_epoch(self):
+        pass
+
+    def training_step(self, batch):
+        pass
+
+    def train_run(self, validation=False):
+        pass
+    
+    def validation_pass(self):
+        pass
+
+    def validation_step(self, batch):
+        pass
+    
+    def log_result(self):
+        pass
